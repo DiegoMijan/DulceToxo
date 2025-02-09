e4caa0a9378fa12a16b90d8cfb3b31264b6a063b
DiegoMijan <dfmijan@gmail.com>
2025-02-09 12:31:58 +0100

    feat: add nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@vueuse/nuxt','@pinia/nuxt','@formkit/auto-animate/nuxt','magic-regexp/nuxt','@nuxtjs/i18n','@nuxt/image','@nuxt/icon'],
 })
