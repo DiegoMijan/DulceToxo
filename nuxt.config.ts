e541ff1a2c07784610f319bd05cac5e845bfe3d0
DiegoMijan <dfmijan@gmail.com>
2025-02-09 13:34:22 +0100

    feat: add tailwind 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@vueuse/nuxt','@pinia/nuxt','@formkit/auto-animate/nuxt','magic-regexp/nuxt','@nuxtjs/i18n','@nuxt/image','@nuxt/icon'],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss() as any,
+    ],
+  },
 })
