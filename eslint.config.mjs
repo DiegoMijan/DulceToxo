6bf0969c7de818c89f2cd2d47d9031b40b44b8d6
DiegoMijan <dfmijan@gmail.com>
2025-08-03 20:59:38 +0200

    feat: actualizar esquema de Biome a la versión 2.1.3, mejorar dependencias en package.json y bun.lock, y agregar configuración de eslint para typed-rocks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import withNuxt from ".nuxt/eslint.config.mjs"
+import typedRocks from "eslint-plugin-typed-rocks"
 import unusedImports from "eslint-plugin-unused-imports"
 
 export default withNuxt({
@@ -11,8 +12,11 @@ export default withNuxt({
     "no-unused-vars": ["error", { vars: "all", args: "none", ignoreRestSiblings: true }],
     "unused-imports/no-unused-imports": "error",
     "vue/no-multiple-template-root": "off",
+    "max-depth": ["error", { max: 3 }],
+    "typed-rocks/max-depth": ["warn", 3],
   },
   plugins: {
     "unused-imports": unusedImports,
+    "typed-rocks": typedRocks,
   },
 })
