73e31f2b5509bbf1dfc3941aedda6e6f84e503cf
DiegoMijan <dfmijan@gmail.com>
2025-08-29 23:47:38 +0200

    refactor: reemplazar la clonación de objetos en useForm con structuredClone y corregir un error tipográfico en las traducciones de gl.json

--- app/composables/useForm.ts
+++ app/composables/useForm.ts
@@ -1,9 +1,9 @@
 export const useForm = <T>(initialValues: T) => {
-   const reactiveForm = ref<T>( { ...initialValues })
+  const reactiveForm = ref<T>(structuredClone(initialValues))
 
-   const reset = () => {
-    reactiveForm.value = { ...initialValues }
-   }
+  const reset = () => {
+    reactiveForm.value = structuredClone(initialValues)
+  }
 
-   return { reactiveForm, reset, }
+  return { reactiveForm, reset }
 }
